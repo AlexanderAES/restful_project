31f172da18616a3c65205d17aafcd902de81af21
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-12 13:44:07 +0300

    add ProductRepository

---
+++ src/main/java/com/alexandersu/market_place_rest/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.alexandersu.market_place_rest.repository;
+
+import com.alexandersu.market_place_rest.entity.Product;
+import com.alexandersu.market_place_rest.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    List<Product> findAllByOrderByCreatedDateDesc();
+    List<Product> findByTitleContainingIgnoreCase(String title);
+    List<Product> findAllByUserOrderByCreatedDateDesc(User user);
+    Optional<Product> findProductByIdAndUser(Long id, User user);
+
+}
