5896fee4b1868f0cb27af2f28fbb0da67ae2759b
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-12 13:46:34 +0300

    add ImageRepository

---
+++ src/main/java/com/alexandersu/market_place_rest/repository/ImageRepository.java
@@ -0,0 +1,12 @@
+package com.alexandersu.market_place_rest.repository;
+
+import com.alexandersu.market_place_rest.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image,Long> {
+    Optional<Image> findByProductId(Long productId);
+}
