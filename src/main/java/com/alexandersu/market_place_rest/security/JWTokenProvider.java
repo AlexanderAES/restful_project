066ac64a0356223cc6a52203b07ec7d814b3b8ee
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-12 18:34:31 +0300

    add JSON Web Tokens

---
+++ src/main/java/com/alexandersu/market_place_rest/security/JWTokenProvider.java
@@ -0,0 +1,61 @@
+package com.alexandersu.market_place_rest.security;
+
+import com.alexandersu.market_place_rest.entity.User;
+import io.jsonwebtoken.*;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Log4j2
+public class JWTokenProvider {
+
+    public String generatedToken(Authentication authentication) {
+        User user = (User) authentication.getPrincipal(); // возвращает объект, поэтому cast
+        Date now = new Date(System.currentTimeMillis()); // берем текущее время
+        Date expiryDate = new Date(now.getTime() + SecurityConstants.EXPIRATION_TIME); // время действия токена сейчас + EXPIRATION_TIME
+
+        String userId = Long.toString(user.getId());
+
+        Map<String, Object> claimsMap = new HashMap<>(); //объект claimsMap будем передавать в json web token-е
+        claimsMap.put("id", userId);
+        claimsMap.put("email", user.getEmail());
+        claimsMap.put("phoneNumber", user.getPhoneNumber());
+        claimsMap.put("name", user.getName());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .addClaims(claimsMap)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET)
+                .compact();
+    }
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(SecurityConstants.SECRET)
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SignatureException |
+                MalformedJwtException |
+                ExpiredJwtException |
+                UnsupportedJwtException |
+                IllegalArgumentException ex) {
+            log.error(ex.getMessage());
+            return false;
+        }
+    }
+    public Long getUserIdFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstants.SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+        String id = (String) claims.get("id");
+        return Long.parseLong(id);
+    }
+}
