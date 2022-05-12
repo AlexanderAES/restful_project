a1bd0385c973d158793f991d5e8ffac8c5b9173f
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-12 11:43:07 +0300

    add settings application.properties connect to data base

---
+++ src/main/java/com/alexandersu/market_place_rest/MarketPlaceRestApplication.java
@@ -0,0 +1,13 @@
+package com.alexandersu.market_place_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlaceRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPlaceRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandersu/market_place_rest/MarketPlaceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandersu.market_place_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPlaceRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
