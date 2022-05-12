15aa7fd280c748d1670d49f3b5c9f6387082b03d
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-12 13:34:16 +0300

    add UserRepository

---
+++ src/main/java/com/alexandersu/market_place_rest/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.alexandersu.market_place_rest.repository;
+
+import com.alexandersu.market_place_rest.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User>findUserByEmail(String email);
+    Optional<User>findUserById(Long id);
+}
