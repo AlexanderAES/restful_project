e61abc2140fef24223bcd502a478b289f25911e7
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-12 12:37:10 +0300

    add annotations Product

--- src/main/java/com/alexandersu/market_place_rest/entity/Product.java
+++ src/main/java/com/alexandersu/market_place_rest/entity/Product.java
@@ -1,21 +1,55 @@
 package com.alexandersu.market_place_rest.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Entity
+@Table(name = "product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "description", columnDefinition = "text")
     private String description;
+
+    @Column(name = "price")
     private int price;
+
+    @Column(name = "city")
     private String city;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
     private Category category;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
     private List<Image> images = new ArrayList<>();
+
     private Long previewImageId;
+
     private LocalDateTime createDate;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @JoinColumn
     private User user;
 
+    @PrePersist
     protected void init() {
         createDate = LocalDateTime.now();
     }
