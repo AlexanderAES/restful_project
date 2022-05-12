d34dc613e9181683751fc6249f00073b90ebf161
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-12 12:40:38 +0300

    add annotations Category

--- src/main/java/com/alexandersu/market_place_rest/entity/Category.java
+++ src/main/java/com/alexandersu/market_place_rest/entity/Category.java
@@ -1,10 +1,26 @@
 package com.alexandersu.market_place_rest.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "category")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "category_name", nullable = false)
     private String categoryName;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "category")
     private List<Product> products = new ArrayList<>();
 }
