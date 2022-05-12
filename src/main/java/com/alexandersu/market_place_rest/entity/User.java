8dda4319e72dfb7a729c8b4abb90325049e6a473
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-12 12:26:05 +0300

    add annotations User

--- src/main/java/com/alexandersu/market_place_rest/entity/User.java
+++ src/main/java/com/alexandersu/market_place_rest/entity/User.java
@@ -1,25 +1,61 @@
 package com.alexandersu.market_place_rest.entity;
 
 import com.alexandersu.market_place_rest.entity.enums.Role;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "email", unique = true)
     private String Email;
+
+    @Column(name = "phone_number", unique = true)
     private String phoneNumber;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(name = "active")
     private boolean active;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "image_id")
     private Image avatar;
+
+    @Column(length = 3000)
     private String password;
+
+    @ElementCollection(targetClass = Role.class)
+    @CollectionTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"))
     private Set<Role> roles = new HashSet<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime createDate;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "user")
     private List<Product> products = new ArrayList<>();
 
+    @PrePersist
     protected void onCreate() {
         this.createDate = LocalDateTime.now();
     }
