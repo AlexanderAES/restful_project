091369511dd024c305ed62b2fc304cc8a993146e
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-12 20:34:04 +0300

    add JWTAuthenticationEntryPoint and InvalidLoginResponse

---
+++ src/main/java/com/alexandersu/market_place_rest/payload/response/InvalidLoginResponse.java
@@ -0,0 +1,14 @@
+package com.alexandersu.market_place_rest.payload.response;
+
+import lombok.Getter;
+
+@Getter
+public class InvalidLoginResponse {
+    private String username;
+    private String password;
+
+    public InvalidLoginResponse() {
+        this.username = "Invalid Username";
+        this.password = "Invalid Password";
+    }
+}
