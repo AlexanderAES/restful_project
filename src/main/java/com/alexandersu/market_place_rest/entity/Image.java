0a81fd7cfe0c8409fc5a7b97f9f518b7f3154c38
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-12 12:17:09 +0300

    add entity Image

--- src/main/java/com/alexandersu/market_place_rest/entity/Image.java
+++ src/main/java/com/alexandersu/market_place_rest/entity/Image.java
@@ -1,4 +1,14 @@
 package com.alexandersu.market_place_rest.entity;
 
 public class Image {
+
+    private Long id;
+    private String name;
+    private String originalFileName;
+    private Long size;
+    private String contentType;
+    private boolean isPreviewImage;
+    private byte[] bytes;
+    private Product product;
+
 }
