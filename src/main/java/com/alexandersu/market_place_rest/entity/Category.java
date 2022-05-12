12d7cf9dab4bb8072e76669692897326402b3f58
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-12 12:14:29 +0300

    add entity Category

--- src/main/java/com/alexandersu/market_place_rest/entity/Category.java
+++ src/main/java/com/alexandersu/market_place_rest/entity/Category.java
@@ -1,4 +1,10 @@
 package com.alexandersu.market_place_rest.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Category {
+    private Long id;
+    private String categoryName;
+    private List<Product> products = new ArrayList<>();
 }
