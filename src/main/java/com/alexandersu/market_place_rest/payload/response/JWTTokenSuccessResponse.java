6c38b3d59329d36e129c7cc0c9ee97dd60deeddc
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-13 14:37:24 +0300

    add JWTTokenSuccessResponse,MessageResponse

---
+++ src/main/java/com/alexandersu/market_place_rest/payload/response/JWTTokenSuccessResponse.java
@@ -0,0 +1,11 @@
+package com.alexandersu.market_place_rest.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JWTTokenSuccessResponse {
+    private boolean success;
+    private String token;
+}
