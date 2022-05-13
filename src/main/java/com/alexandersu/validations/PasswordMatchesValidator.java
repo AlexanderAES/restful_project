fe6094d6018e817131925e134211993647e14f63
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-13 09:50:22 +0300

    add custom annotation PasswordMatches and PasswordMatchesValidator

---
+++ src/main/java/com/alexandersu/validations/PasswordMatchesValidator.java
@@ -0,0 +1,20 @@
+package com.alexandersu.validations;
+
+import com.alexandersu.annotations.PasswordMatches;
+import com.alexandersu.market_place_rest.payload.request.SignupRequest;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+    @Override
+    public void initialize(PasswordMatches constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(Object obj, ConstraintValidatorContext constraintValidatorContext) {
+        SignupRequest userSignupRequest = (SignupRequest) obj;
+        return userSignupRequest.getPassword().equals(userSignupRequest.getConfirmPassword());
+    }
+}
