5031d68a8caca0cdfd12277dff5de6e48d5a0e81
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-13 07:56:04 +0300

    add payload model LoginRequest, SignupRequest

---
+++ src/main/java/com/alexandersu/market_place_rest/payload/request/SignupRequest.java
@@ -0,0 +1,27 @@
+package com.alexandersu.market_place_rest.payload.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+public class SignupRequest {
+
+    @Email(message = "It should have email format")
+    @NotBlank(message = "User email is required")
+    private String email;
+
+    @NotEmpty(message = "Please enter your phone number")
+    private String phoneNumber;
+
+    @NotEmpty(message = "Please enter your name")
+    private String name;
+
+    @NotEmpty(message = "Password is required")
+    @Size(min = 6)
+    private String password;
+    private  String confirmPassword;
+}
