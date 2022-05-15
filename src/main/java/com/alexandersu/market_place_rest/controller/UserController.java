bb95a393e4660d63d7f1c4f3b7e45e76ff63f608
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-15 14:31:41 +0300

    add methods in to UserController: getCurrentUser, getUserProfile, updateUser

--- src/main/java/com/alexandersu/market_place_rest/controller/UserController.java
+++ src/main/java/com/alexandersu/market_place_rest/controller/UserController.java
@@ -1,5 +1,57 @@
 package com.alexandersu.market_place_rest.controller;
 
+import com.alexandersu.market_place_rest.dto.UserDTO;
+import com.alexandersu.market_place_rest.entity.User;
+import com.alexandersu.market_place_rest.mappers.UserMapper;
+import com.alexandersu.market_place_rest.service.UserService;
+import com.alexandersu.market_place_rest.validations.ResponseErrorValidation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
+
+@RestController
+@RequestMapping("api/users")
+@CrossOrigin
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+    private final ResponseErrorValidation responseErrorValidation;
+
+    // метод возвращает пользователя который находится авторизированным в системе
+    @GetMapping("/")
+    public ResponseEntity<UserDTO> getCurrentUser(Principal principal) {
+        User user = userService.getCurrentUser(principal);
+        UserDTO userDTO = UserMapper.INSTANCE.toDTO(user);
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    // метод возвращает пользователя по его id
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserProfile(@PathVariable("userId") String userId) {
+        User user = userService.getUserById(Long.parseLong(userId));
+        UserDTO userDTO = UserMapper.INSTANCE.toDTO(user);
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+    // метод для изменения профиля пользователя
+    @PutMapping("/update")
+    public ResponseEntity<Object> updateUser(@Valid @RequestBody UserDTO userDTO,
+                                             BindingResult bindingResult,
+                                             Principal principal) {
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if (!ObjectUtils.isEmpty(errors)) return errors;
+
+        User user = userService.updateUser(userDTO, principal);
+
+        UserDTO userUpdated = UserMapper.INSTANCE.toDTO(user);
+        return new ResponseEntity<>(userUpdated, HttpStatus.OK);
+    }
 
 }
