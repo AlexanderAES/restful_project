62609b59b0b094406dba678c77a3e095fa2a6b74
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-17 19:04:48 +0300

    add methods in to ProductController: createProduct,getProductInfo,deleteProduct,getListProduct,getAllProductsForUser,updateProduct

--- src/main/java/com/alexandersu/market_place_rest/controller/ProductController.java
+++ src/main/java/com/alexandersu/market_place_rest/controller/ProductController.java
@@ -4,7 +4,9 @@ package com.alexandersu.market_place_rest.controller;
 import com.alexandersu.market_place_rest.dto.ProductDTO;
 import com.alexandersu.market_place_rest.entity.Product;
 import com.alexandersu.market_place_rest.mappers.ProductMapper;
+import com.alexandersu.market_place_rest.payload.response.MessageResponse;
 import com.alexandersu.market_place_rest.service.ProductService;
+import com.alexandersu.market_place_rest.service.UserService;
 import com.alexandersu.market_place_rest.validations.ResponseErrorValidation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/product")
@@ -25,15 +29,19 @@ import java.security.Principal;
 public class ProductController {
 
     private final ProductService productService;
+    private final UserService userService;
     private final ResponseErrorValidation responseErrorValidation;
 
 
+    // создание продукта
     @PostMapping("/create")
-    public ResponseEntity<Object>createProduct(@Valid @RequestBody ProductDTO productDTO,
-                                               BindingResult bindingResult,
-                                               Principal principal){
+    public ResponseEntity<Object> createProduct(@Valid @RequestBody
+                                                        ProductDTO productDTO,
+                                                BindingResult bindingResult,
+                                                Principal principal) {
         ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
         if (!ObjectUtils.isEmpty(errors)) return errors;
+
         Product product = productService.createProduct(productDTO, principal);
 
         ProductDTO createdProduct = ProductMapper.INSTANCE.ProductToProductDTO(product);
@@ -41,4 +49,59 @@ public class ProductController {
 
         return new ResponseEntity<>(createdProduct, HttpStatus.OK);
     }
+
+    // информация о продукте по его id
+    @GetMapping("/info/{productId}")
+    public ResponseEntity<ProductDTO> getProductInfo(@PathVariable("productId") String productId) {
+        Product product = productService.getProductById(Long.parseLong(productId));
+        ProductDTO productDTOInfo = ProductMapper.INSTANCE.ProductToProductDTO(product);
+        return new ResponseEntity<>(productDTOInfo, HttpStatus.OK);
+    }
+
+    // удаление продукта по его id
+    @DeleteMapping("/delete/{productId}")
+    public ResponseEntity<MessageResponse> deleteProduct(@PathVariable("productId") String productId) {
+        productService.deleteProduct(Long.parseLong(productId));
+        log.info("Delete product with id {}", productId);
+        return new ResponseEntity<>(new MessageResponse("Product was deleted"), HttpStatus.OK);
+    }
+
+    // поиск продукта по ключевому слову
+    @GetMapping("/search/{title}")
+    public ResponseEntity<List<ProductDTO>> getListProduct(@PathVariable("title") String title) {
+        List<ProductDTO> productDTOList = productService.getListProducts(title)
+                .stream()
+                .map(ProductMapper.INSTANCE::ProductToProductDTO)
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(productDTOList, HttpStatus.OK);
+    }
+
+    // список всех продуктов пользователя
+    @GetMapping("/user/products")
+    public ResponseEntity<List<ProductDTO>> getAllProductsForUser(Principal principal) {
+        List<ProductDTO> productDTOList = productService.getAllProductFromUser(principal)
+                .stream()
+                .map(ProductMapper.INSTANCE::ProductToProductDTO)
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(productDTOList, HttpStatus.OK);
+    }
+
+    // редактирование объявления поьзователем при условии что это его объявление
+    @PutMapping("/update/{productId}")
+    public ResponseEntity<Object> updateProduct(@PathVariable("productId") String productId, @Valid @RequestBody
+            ProductDTO productDTO, BindingResult bindingResult, Principal principal) {
+
+        Product product = productService.updateProduct(productDTO, principal, productId);
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+
+        if (!ObjectUtils.isEmpty(errors)) return errors;
+        ProductDTO productUpdated = ProductMapper.INSTANCE.ProductToProductDTO(product);
+
+        if (productUpdated != null) return new ResponseEntity<>(productUpdated, HttpStatus.OK);
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("The product has not been updated");
+
+    }
 }
+
+
