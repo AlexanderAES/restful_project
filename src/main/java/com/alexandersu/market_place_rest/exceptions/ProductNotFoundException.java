6d6294f4bbe89f13f0a404832019bd80529ad738
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-17 08:23:38 +0300

    add ProductNotFoundException

---
+++ src/main/java/com/alexandersu/market_place_rest/exceptions/ProductNotFoundException.java
@@ -0,0 +1,11 @@
+package com.alexandersu.market_place_rest.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(Long id) {
+        super(String.format("Объявления с таким id '%s' не найдено", id));
+    }
+}
