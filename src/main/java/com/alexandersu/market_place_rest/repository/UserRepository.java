6da27a3078bf44a1a8dcadb1f9e35f8ea6508131
Alexander Suetnov <aes123.aa@gmail.com>
2022-05-18 12:55:07 +0300

    add findUserIdByEmail in to UserRepository, for possibility ban user by id

--- src/main/java/com/alexandersu/market_place_rest/repository/UserRepository.java
+++ src/main/java/com/alexandersu/market_place_rest/repository/UserRepository.java
@@ -11,6 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User>findUserByEmail(String email);
     Optional<User>findUserById(Long id);
-    User findByEmail(String email);
+    User findUserIdByEmail(String email);
     Optional<User> findUserByUsername(String username);
 }
